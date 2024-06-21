b78d30d1388a709845e4c165343fb00b728fb419
wsw <wxz0129@126.com>
2024-06-21 17:56:30 +0800

    emplace_back

---
+++ my_little_test/emplace_back.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+#include <vector>
+#include <cassert>
+#include <iostream>
+#include <string>
+ 
+struct President
+{
+    std::string name;
+    std::string country;
+    int year;
+ 
+    President(std::string p_name, std::string p_country, int p_year)
+        : name(std::move(p_name)), country(std::move(p_country)), year(p_year)
+    {
+        std::cout << "I am being constructed.\n";
+    }
+ 
+    President(President&& other)
+        : name(std::move(other.name)), country(std::move(other.country)), year(other.year)
+    {
+        std::cout << "I am being moved.\n";
+    }
+ 
+    President& operator=(const President& other) = default;
+};
+ 
+int main()
+{
+    std::vector<President> elections;
+    std::cout << "emplace_back:\n";
+    auto& ref = elections.emplace_back("Nelson Mandela", "South Africa", 1994);
+    assert(ref.year == 1994 && "uses a reference to the created object (C++17)");
+//-------------------------------------------
+    std::vector<President> elections2;
+    std::cout << endl << "std::move" <<endl;
+    //再用一次std::move是没有意义的，因为push_back默认就会先调用右值引用的函数
+    elections2.push_back(std::move(President("Nelson Mandela", "South Africa", 1994))); 
+//------------------------------------------
+    std::vector<President> reElections;
+    std::cout << "\npush_back:\n";
+    reElections.push_back(President("Franklin Delano Roosevelt", "the USA", 1936));
+ 
+    std::cout << "\nContents:\n";
+    for (President const& president: elections)
+        std::cout << president.name << " was elected president of "
+                  << president.country << " in " << president.year << ".\n";
+ 
+    for (President const& president: reElections)
+        std::cout << president.name << " was re-elected president of "
+                  << president.country << " in " << president.year << ".\n";
+}
